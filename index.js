cdf6188a276996a0d03d7eebc7874985c64273b5
Mike <theonlybuschi@gmail.com>
2023-04-10 17:16:23 +0200

    Automatisches Ausführen der Middlewares

--- index.js
+++ index.js
@@ -23,12 +23,15 @@ server.set("view engine", "ejs");
 
 server.get('/', (req, res) => res.render('pages/index'))
 
+//Deutscher Urpsrungstext vom Frontend geht an diese Middleware
 server.post('/api/text', (req, res, next) => {
     savedText = req.body.text;
     console.log("Eingangstext: " +savedText);
+    //next(); ruft automatisch die nächste Middleware zur Übersetzung auf
     next();
 })
 
+//Diese Middleware übersetzt den Text und sendet diesen an das Frontend
 server.use(async (req, res) => {
     translatedText = await Deepl(savedText)
     console.log("Übersetzter Text: " +translatedText);
@@ -37,4 +40,18 @@ server.use(async (req, res) => {
         text: translatedText
     });
 });
+
+//Der übersetzte und ggf. abgeänderte Text vom Frontend geht an diese Middleware
+server.post('/api/translated-text', (req, res, next) => {
+    translatedText = req.body.text;
+    console.log("Zu korrigierender Text: " +translatedText);
+    next();
+})
+
+//Diese Middleware korrigiert den übersetzten Text
+server.use(async (req, res) => {});
+
+
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
